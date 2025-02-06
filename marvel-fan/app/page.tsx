ca337ad70e34dd11ec5c3e89b6644fe636ba34ed
Bruno <bruno.ferraz@atos.net>
2025-02-06 10:06:27 -0300

    feat: background animation

--- marvel-fan/app/page.tsx
+++ marvel-fan/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import profilePic from "../public/images/foto.jpeg";
 import TechList from "@/components/tech-list/tech-list";
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -12,8 +13,8 @@ export default function Home() {
         <Image
           src={profilePic}
           alt={"Picture"}
-          width={300}
-          height={300}
+          width={500}
+          height={500}
           className={styles.imagePic} />
         <div className={styles.apresentation}>
           <h2>Bruno Henrique</h2>
@@ -24,8 +25,12 @@ export default function Home() {
 
       <div className={styles.container2}>
         <h1>Tecnologias</h1>
-      </div><div className={styles.techList}>
+      </div>
+      <div className={styles.techList}>
         <TechList />
+      </div>
+      <div className={styles.container3}>
+        <h1>Projetos</h1>
       </div>
       </>
   );
--- marvel-fan/components/tech-list/tech-list.tsx
+++ marvel-fan/components/tech-list/tech-list.tsx
@@ -6,16 +6,15 @@ import styles from "./tech-list.module.css";
 export default function TechList() {
 
     return (
-        <div className={styles.container}>
-            {icons.map((item) => (
-                 <div key={item.id}>
-                 <Image
-                   src={item.img}
-                   alt={item.title}
-                   className={styles.techIcons}
-                 />
-               </div>
-            ))}
+      <div className={styles.container}>
+      {icons.map((item, index) => (
+        <div key={item.id} className={`${styles.iconWrapper} ${index % 2 === 0 ? styles.lower : styles.higher}`}>
+          <Image src={item.img} alt={item.title} className={styles.techIcons} />
+          <p className={styles.iconTitle}>{item.title}</p>
         </div>
+      ))}
+    </div>
+    
+    
     );
 }
